d6807b98a8ee47c2f03ce3b54c1baf15bbb4aeaa
Zizheng Wu <me@zizhengwu.com>
2016-04-10 20:29:22 -0400

    at least I am able to call API

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,14 +4,9 @@ class RestaurantsController < ApplicationController
 
   PER_PAGE = 10
 
-  def self.dataset
-    @dataset ||= Gcloud.datastore(
-        Rails.application.config.database_configuration[Rails.env]['dataset_id']
-    )
-  end
-
   def index
     @restaurants, @cursor = Restaurant.query limit: PER_PAGE, cursor: params[:cursor]
+    puts 'hi'
   end
 
   def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,25 @@
-class Restaurant < ActiveRecord::Base
+require "gcloud/datastore"
+
+class Restaurant
+  def self.dataset
+    @dataset ||= Gcloud.datastore(
+        Rails.application.config.database_configuration[Rails.env]['dataset_id']
+    )
+  end
+
+  def self.query options = {}
+    query = Gcloud::Datastore::Query.new
+    query.kind "Business"
+    query.limit options[:limit]   if options[:limit]
+    query.cursor options[:cursor] if options[:cursor]
+
+    results = dataset.run query
+    restaurants   = results.map {|entity| Book.from_entity entity }
+
+    if options[:limit] && results.size == options[:limit]
+      next_cursor = results.cursor
+    end
+
+    return restaurants, next_cursor
+  end
 end
